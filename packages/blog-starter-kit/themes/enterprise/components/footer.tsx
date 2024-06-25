5dc172d27a1c00ff040558283d4fd7726dc10c4c
semagtdi <168283333+semagtdi@users.noreply.github.com>
2024-06-26 04:45:35 +0800

    rtea

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -8,22 +8,7 @@ export const Footer = () => {
 	const PUBLICATION_LOGO = publication.preferences.logo;
 	return (
 		<footer className="border-t py-20 dark:border-neutral-800 ">
-			<Container className="px-5">
-				{PUBLICATION_LOGO ? (
-					<div className="mb-20 flex w-full flex-row justify-center">
-						<Link
-							href={'/'}
-							aria-label={`${publication.title} home page`}
-							className="flex flex-row items-center gap-5"
-						>
-							<img className="block w-40" src={PUBLICATION_LOGO} alt={publication.title} />
-						</Link>
-					</div>
-				) : (
-					<p className="mb-20 text-center text-xl font-semibold text-slate-900 dark:text-slate-50 md:text-4xl">
-						{publication.title}
-					</p>
-				)}
+			<Container className="px-5">		
 				<div className="grid w-full grid-cols-3 gap-5 md:grid-cols-6 lg:grid-cols-5">
 					<div className="col-span-1 grid grid-cols-4 gap-5 md:col-span-4 lg:col-span-3">
 						<div className="col-span-full md:col-span-2 lg:col-span-1">
--- packages/blog-starter-kit/themes/enterprise/components/more-posts.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/more-posts.tsx
@@ -11,7 +11,7 @@ export const MorePosts = ({ posts, context }: Props) => {
 		<section className="mb-10 flex flex-col items-start gap-10">
 			{context === 'home' && (
 				<h2 className="text-xl font-bold leading-tight tracking-tight text-slate-900 dark:text-neutral-50 lg:text-3xl">
-					More Posts
+					更多文章
 				</h2>
 			)}
 			<div className="grid items-start gap-5 md:grid-cols-2 xl:grid-cols-3">
